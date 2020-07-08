5944b9990fe73eda7c12548efcff7881f656e57f
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-09 01:30:55 +0300

    Добавьте файлы проекта.

---
+++ pract8_/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace pract8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"ЭтО just one НебольШой message for простого cheking this проГрамМЫ";
+            Console.WriteLine("The original message:\n " + text);
+            var newText = Regex.Replace(text, "[абвгдеёжзийклмнопрстуфхцчшщыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ]", "");
+            Console.WriteLine("Changed message:\n " + newText);
+            Console.ReadLine();
+        }
+    }
+}
